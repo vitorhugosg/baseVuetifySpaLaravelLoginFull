5e110a0b806ca41226d4d681818995de27a29fd8
Vitor Hugo <vitorhugosg@gmail.com>
2019-02-03 21:29:22 -0200

    Segue arrumação do blog

--- backend/app/Http/Controllers/blog/indexController.php
+++ backend/app/Http/Controllers/blog/indexController.php
@@ -9,6 +9,6 @@ use App\Post;
 class indexController extends Controller
 {
     public function index(Request $request){
-    	return Post::published()->status('publish')->paginate(5);
+    	return Post::published()->status('publish')->paginate(8);
     }
 }
--- frontend/src/components/sections/blog/blogfull.vue
+++ frontend/src/components/sections/blog/blogfull.vue
@@ -1,71 +1,29 @@
 <template>
-  <v-layout v-if="posts">
-    <v-flex sm3 v-for="post in posts" :key="post.ID">
-      <v-card class="m-3">
-        <div v-if="post.thumbnail != null">
-          <v-img class="white--text" height="200px" :src="post.thumbnail.attachment.url"/>
-        </div>
-        <div v-else>
-          <v-img
-            class="white--text"
-            height="200px"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          />
-        </div>
-
-        <v-container fill-height fluid>
-          <v-layout fill-height>
-            <v-flex xs12 align-end flexbox>
-              <span class="headline">{{post.title | truncate}}</span>
-            </v-flex>
-          </v-layout>
-        </v-container>
-        <v-card-text v-html="truncateBody(post.content)" class="truncateBlog"></v-card-text>
-        <v-card-actions>
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-md-12">
-                <v-btn block dark color="primary">Ver notícia</v-btn>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12">
-                  <!-- social buttons -->
-              </div>
-            </div>
-          </div>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12 text-center py-4" >
+          <h2 class="font-weight-medium primary--text">Ultimas notícias do blog</h2>
+      </div>
+    </div>
+    <div class="row" v-if="posts">
+      <div class="col-sm-12 col-md-6 col-lg-3" v-for="post in posts" :key="post.ID">
+        <blogCard :post="post"></blogCard>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import blogCard from "@/components/cards/blog/cardBlog";
 export default {
+  components: {
+    blogCard
+  },
   created() {
     this.$store.dispatch("GET_POSTS");
-    setTimeout(() => {
-      console.log(this.posts);
-    }, 2000);
-  },
-  methods: {
-    truncateBody: function(value) {
-      if (value.length > 72) {
-        value = value.substring(0, 69) + "...";
-      }
-      return value;
-    }
   },
   computed: {
     posts() {
       return this.$store.getters.getPosts;
-    }
-  },
-  filters: {
-    truncate: function(value) {
-      if (value.length > 14) {
-        value = value.substring(0, 11) + "...";
-      }
-      return value;
     }
   }
 };
@@ -78,9 +36,9 @@ export default {
   padding: 0px !important;
   margin: 0px !important;
 }
-.truncateBlog.v-card__text{
-    padding: 0px 16px 0px !important;
-    
-  height: 50px;
+.truncateBlog.v-card__text {
+  padding: 0px 16px 0px !important;
+
+  height: 55px;
 }
 </style>
